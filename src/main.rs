6114aa8dcf15c60527923b3d6fb3243ecb24eed0
Devsstudent <devsstudent777@gmail.com>
2024-10-06 15:01:55 +0200

    started to play with route axum

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,27 @@
-use axum::Router;
+use axum::{
+    routing::{
+		get,
+		post
+	},
+    Router,
+	Json,
+	response::IntoResponse,
+};
+use serde::{Deserialize, Serialize};
 
 pub mod configs;
 pub mod simulator;
+
+#[derive(Deserialize, Serialize)]
+struct InputData {
+    name: String,
+    age: u8,
+}
+
+async fn recv_t(Json(payload): Json<InputData>) -> String {
+    format!("Received name: {}, age: {}", payload.name, payload.age)
+}
+
 
 #[tokio::main]
 async fn main() {
@@ -13,10 +33,12 @@ async fn main() {
 
     // build our application with a route
     // TODO: add our endpoint to receive transactions
-    let app = Router::new();
+    let app = Router::new()
+		.route("/hi", post(recv_t));
 
     // run our app with hyper, listening globally on port 3000
     let addr = format!("{}:{}", config.server.host, config.server.port);
     let listener = tokio::net::TcpListener::bind(addr).await.expect("port to be free");
     axum::serve(listener, app).await.expect("server to start");
 }
+
